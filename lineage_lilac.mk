9d9ed4716c5c0fb69c6764d704058cc639c84ca9
Alexander Grund <Flamefire@users.noreply.github.com>
2021-07-14 18:27:01 +0200

    lilac: Update to July pixel FP
    Thanks @shank03
    
    Change-Id: I833b6ea6cb3d1791cff4720d98bd0c20da00a363

--- lineage_lilac.mk
+++ lineage_lilac.mk
@@ -27,11 +27,11 @@ PRODUCT_MANUFACTURER := Sony
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=lilac \
-    PRIVATE_BUILD_DESC="redfin-user 11 RQ3A.210605.005 7349499 release-keys" \
-    BUILD_NUMBER=7349499
+    PRIVATE_BUILD_DESC="redfin-user 11 RQ3A.210705.001 7380771 release-keys" \
+    BUILD_NUMBER=7380771
 
-# Pixel 5 June fingerprint
-BUILD_FINGERPRINT := google/redfin/redfin:11/RQ3A.210605.005/7349499:user/release-keys
+# Pixel 5 July fingerprint
+BUILD_FINGERPRINT := google/redfin/redfin:11/RQ3A.210705.001/7380771:user/release-keys
 
 PRODUCT_GMS_CLIENTID_BASE := android-sony-mobile
 
