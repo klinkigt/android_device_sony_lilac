8a20f8c3dad96de7d73a4533731d4cf5ed468611
Alexander Grund <Flamefire@users.noreply.github.com>
2021-08-13 11:52:16 +0200

    Add new patches to fix bugs and allow compilation with Clang 11
    Change-Id: I20bd22829944644d56fa800b676d44c798ec0a47

--- patches/applyPatches.sh
+++ patches/applyPatches.sh
@@ -12,19 +12,59 @@ function do_patch {
 
 PATCH_ROOT="$ANDROID_BUILD_TOP/device/sony/lilac/patches"
 
+cd "$ANDROID_BUILD_TOP/art"
+do_patch "update-cpp-define-generator.patch"
+do_patch "use-uxtw-instead-of-lsl-for-asm.patch"
+
+cd "$ANDROID_BUILD_TOP/bionic"
+do_patch "add-missing-nothrow.patch"
+do_patch "dont-export-libm-libc-symbols.patch"
+do_patch "remove-fallhthrough-warning.patch"
+
+cd "$ANDROID_BUILD_TOP/build/soong"
+do_patch "make-stub-symbols-visible.patch"
+do_patch "update-clang.patch"
+
+cd "$ANDROID_BUILD_TOP/external/libunwind_llvm"
+do_patch "fix-llvm-unwind-warning-and-findUnwindSections.patch"
+
 cd "$ANDROID_BUILD_TOP/external/selinux"
 do_patch "fix_audit2allow.patch"
 
+cd "$ANDROID_BUILD_TOP/frameworks/av"
+do_patch "fix-mediaplayer2-flags.patch"
+
 cd "$ANDROID_BUILD_TOP/frameworks/base"
 do_patch "fix-null-ptr-in-customtile.patch"
+do_patch "fix-reversed-logic.patch"
 
 cd "$ANDROID_BUILD_TOP/frameworks/native"
+do_patch "fix-native-vulkan.patch"
 do_patch "workaround_egl_lib_symbols.patch"
+
+cd "$ANDROID_BUILD_TOP/libnativehelper"
+do_patch "fix-class-templates.patch"
+
+cd "$ANDROID_BUILD_TOP/prebuilts/abi-dumps/vndk"
+do_patch "update-abi-dumps.patch"
 
 cd "$ANDROID_BUILD_TOP/system/core"
 do_patch "fix_snet_probs.patch"
 
+cd "$ANDROID_BUILD_TOP/system/keymaster"
+do_patch "fix-fallthrough-in-keymaster.patch"
+
+cd "$ANDROID_BUILD_TOP/system/libhidl"
+do_patch "fix-setLocked-signature.patch"
+
 cd "$ANDROID_BUILD_TOP/system/security"
 do_patch "0001-keystore-Block-key-attestation-for-Google-Play-Servi.patch"
+
+cd "$ANDROID_BUILD_TOP/system/tools/hidl"
+do_patch "make-hidl_enum_values-inline.patch"
+do_patch "suppress-bool-operation-warning.patch"
+
+cd "$ANDROID_BUILD_TOP/vendor/qcom/opensource/audio"
+do_patch "fix-dangling-pointer.patch"
 
 echo "Done."
