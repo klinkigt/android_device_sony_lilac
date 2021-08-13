3134792bfd2445580e0424920a2059621e98999d
Alexander Grund <Flamefire@users.noreply.github.com>
2021-08-13 11:52:16 +0200

    Add new patches
    Change-Id: I20bd22829944644d56fa800b676d44c798ec0a47

--- patches/applyPatches.sh
+++ patches/applyPatches.sh
@@ -12,19 +12,25 @@ function do_patch {
 
 PATCH_ROOT="$ANDROID_BUILD_TOP/device/sony/lilac/patches"
 
+cd "$ANDROID_BUILD_TOP/system/security"
+do_patch "0001-keystore-Block-key-attestation-for-Google-Play-Servi.patch"
+
 cd "$ANDROID_BUILD_TOP/external/selinux"
 do_patch "fix_audit2allow.patch"
-
-cd "$ANDROID_BUILD_TOP/frameworks/base"
-do_patch "fix-null-ptr-in-customtile.patch"
-
-cd "$ANDROID_BUILD_TOP/frameworks/native"
-do_patch "workaround_egl_lib_symbols.patch"
 
 cd "$ANDROID_BUILD_TOP/system/core"
 do_patch "fix_snet_probs.patch"
 
-cd "$ANDROID_BUILD_TOP/system/security"
-do_patch "0001-keystore-Block-key-attestation-for-Google-Play-Servi.patch"
+cd "$ANDROID_BUILD_TOP/vendor/qcom/opensource/audio"
+do_patch "fix-dangling-pointer.patch"
+
+cd "$ANDROID_BUILD_TOP/frameworks/base"
+do_patch "fix-null-ptr-in-customtile.patch"
+
+cd "$ANDROID_BUILD_TOP/build/soong"
+do_patch "update-clang.patch"
+
+cd "$ANDROID_BUILD_TOP/frameworks/native"
+do_patch "workaround_egl_lib_symbols.patch"
 
 echo "Done."
