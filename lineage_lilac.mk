ade8d15324abb4cde7f3a3cfa18aade709ef2090
Alexander Grund <flamefire89@gmail.com>
2021-10-16 22:16:25 +0200

    lilac: Update to October FP
    Change-Id: I40685c6b35d36a97f88d323ffb3e200a899146a5

--- lineage_lilac.mk
+++ lineage_lilac.mk
@@ -27,11 +27,13 @@ PRODUCT_MANUFACTURER := Sony
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=lilac \
-    PRIVATE_BUILD_DESC="redfin-user 11 RQ3A.210905.001 7511028 release-keys" \
-    BUILD_NUMBER=7511028
+    PRIVATE_BUILD_DESC="redfin-user 11 RQ3A.211001.001 7641976 release-keys" \
+    BUILD_NUMBER=7641976 \
+    VENDOR_SECURITY_PATCH=2019-10-05 \
+    PLATFORM_SECURITY_PATCH=2021-10-01
 
-# Pixel 5 September fingerprint
-BUILD_FINGERPRINT := google/redfin/redfin:11/RQ3A.210905.001/7511028:user/release-keys
+# Pixel 5 October fingerprint
+BUILD_FINGERPRINT := google/redfin/redfin:11/RQ3A.211001.001/7641976:user/release-keys
 
 PRODUCT_GMS_CLIENTID_BASE := android-sony-mobile
 
